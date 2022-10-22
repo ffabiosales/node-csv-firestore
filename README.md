df6cdf24691ab248f7858a5ca90ad4913ea3d5d5
Fábio Sales <ffabiosales@gmail.com>
2022-10-22 13:21:10 -0300

    initial version

---
+++ README.md
@@ -0,0 +1,24 @@
+# CSV PARSE/IMPORT to Firestore
+
+This script parse data from a csv and import to Fire.
+
+> Estimated time to complete: 30min
+
+# Instructions
+
+1.  Clone this repository
+2.  Check your enviroment and gcloud settings. You'll need to set projectID and login from gcloud to be able to import data to firestore from this script.
+
+```
+npm install
+
+```
+
+```
+node index.js
+
+```
+
+## Issues and Questions
+
+> feel free to open issues with your questions and suggestions
---
+++ index.js
@@ -0,0 +1,46 @@
+const CSVToJSON = require("csvtojson");
+const { Firestore } = require("@google-cloud/firestore");
+const DB = new Firestore();
+/**
+ * @function
+ * @param {Object} data
+ * @TODO use batch for bulk insert.
+ */
+async function updateFirestore(data) {
+  for (const [key] of Object.entries(data)) {
+    let country = {
+      name: `${key}`,
+    };
+
+    let document = await DB.collection("countries").add(country);
+
+    for (const key in value) {
+      await DB.collection("countries")
+        .doc(document.id)
+        .collection("cities")
+        .add({
+          name: value[key].name,
+        });
+    }
+  }
+  console.log("Finished!");
+}
+
+// parse csv data to JSON
+(async () => {
+  try {
+    const json = await CSVToJSON().fromFile("./data/world-cities.csv");
+    const key = "country";
+
+    // Group cities by country
+    const countries = json.reduce((acc, item) => {
+      if (!acc[item[key]]) acc[item[key]] = [];
+      acc[item[key]].push(item);
+      return acc;
+    }, {});
+
+    updateFirestore(countries);
+  } catch (err) {
+    console.log(err);
+  }
+})();
